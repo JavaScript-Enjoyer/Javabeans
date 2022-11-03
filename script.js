1fbdc5fb52949272d9763f7cde8e4eedee11dbbd
Java Beans <117130762+UnblockedDev@users.noreply.github.com>
2022-11-02 21:08:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function useCalculator() {
 
+    let num1 = prompt("First Number");
+    num1 = Number(num1);
+    const operator = prompt("Operator (+, -, *, /)");
+    let num2 = prompt("Second Number");
+    num2 = Number(num2);
+  
+    if (operator === "+") {
+      alert(`The answer is ${Number(num1) + Number(num2)}`);
+    } else if (operator === "-") {
+      alert(`The answer is ${Number(num1) - Number(num2)}`);
+    } else if (operator === "*") {
+      alert(`The answer is ${Number(num1) * Number(num2)}`);
+    } else if (operator === "/") {
+      alert(`The answer is ${Number(num1) / Number(num2)}`);
+    }
+  }
